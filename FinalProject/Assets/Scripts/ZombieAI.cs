639ea2d18ed5b7766aa975253050b440e8f4655f
Elizabeth Hendren <eah415@nyu.edu>
2013-11-18 23:05:50 -0500

    This is the second prototype. The zombie and weapon spawn has still not been implemented and the AI script hasn't been tested

---
+++ FinalProject/Assets/Scripts/ZombieAI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ZombieAI : MonoBehaviour {
+	
+		GameObject closestPlayer;
+		GameObject[] players;
+		float closestDistance = 1000000f;
+		
+		
+//	void OnCollisionEnter(Collision collision){
+//		
+//		if (collision.gameObject == CompareTag("Player")){
+//			DestroyObject (GameObject.FindGameObjectsWithTag("Player"));
+//		}
+//	}
+
+	// Use this for initialization
+	void Start () {
+		players = GameObject.FindGameObjectsWithTag("Player");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		foreach (GameObject player in players){
+
+		Vector3 distance = player.transform.position - transform.position;
+
+		if (distance.magnitude < closestDistance){
+		closestPlayer = player;
+		closestDistance = distance.magnitude;
+		transform.Translate(0f  * Time.deltaTime, 0f  * Time.deltaTime, 1f  * Time.deltaTime);
+			}
+			
+		transform.LookAt (player.transform.localPosition);
+			
+		}
+	}
+}
